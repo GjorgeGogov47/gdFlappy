16f131a32c738b2b98a205635df01b3b915f241a
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-06-26 18:09:48 +0200

    finally

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -1,24 +1,17 @@
 extends CanvasLayer
 
 signal restart
+var achievedScore : int
+var lastPlaceScore : int
 
 func _on_restart_button_pressed():
 	restart.emit()
 
-func _ready():
-	for i in range(1,1000):
-		$ColorRect/leaderBoardScroll/leaderBoardLabel.text += str(i) +  ". boyyy\n"
-	#$HTTPRequest.request_completed.connect(_on_request_completed)
-	#$HTTPRequest.request("https://nagrujofunkcijata47.azurewebsites.net/api/GetItems")
-#
-#func _on_request_completed(result, response_code, headers, body):
-	#var json = JSON.parse_string(body.get_string_from_utf8())
-	#for row in json:
-		#$ScrollContainer/Label.text += "ID: " + row["id"] + "    Title: " + row["title"] + "\n"
-	#
-
-
 func _on_leaderboards_button_pressed():
+	if !$HTTPRequest.request_completed.is_connected(_on_get_request_completed):
+		$HTTPRequest.request_completed.connect(_on_get_request_completed)
+	$HTTPRequest.request("https://nagrujofunkcijata.azurewebsites.net/api/GetRows")
+	
 	$ColorRect.size.y = 481
 	$ColorRect.position.y = 79
 	
@@ -32,8 +25,6 @@ func _on_leaderboards_button_pressed():
 	$ColorRect/leaderBoardScroll.show()
 	$ColorRect/RefreshButton.show()
 	$ColorRect/scoreCalculationLabel.hide()
-	
-
 
 func _on_enter_score_button_pressed():
 	$ColorRect/RestartButton.hide()
@@ -58,6 +49,20 @@ func _on_enter_score_button_pressed():
 	$ColorRect/LeaderboardsButton.position.y = 168
 
 func _on_enter_username_button_pressed():
+	$ColorRect/EnterUsernameButton.disabled = true
+	$ColorRect/EnterScoreButton.disabled = true
+	if (lastPlaceScore == 0 or lastPlaceScore < achievedScore) and $ColorRect/UserTextInput.text != "":
+		if !$HTTPRequest.request_completed.is_connected(_on_post_request_completed):
+			$HTTPRequest.request_completed.connect(_on_post_request_completed)
+		var json = JSON.stringify(
+		{
+			"playerName": $ColorRect/UserTextInput.text,
+			"playerScore": achievedScore
+		})
+		var headers = ["Content-Type: application/json"]
+		$HTTPRequest.request("https://nagrujofunkcijata.azurewebsites.net/api/InsertRow", headers, HTTPClient.METHOD_POST, json)
+	
+	
 	$ColorRect/RestartButton.show()
 	$ColorRect/LeaderboardsButton.show()
 	$ColorRect/EnterScoreButton.show()
@@ -68,10 +73,11 @@ func _on_enter_username_button_pressed():
 	$ColorRect.size.y = 224
 	$ColorRect.position.y = 360
 	
-	#JSON
+	$ColorRect/UserTextInput.text=""
+	
 
 func getGameScoreAndReset(gameOverScore):
-	
+	achievedScore = gameOverScore
 	$ColorRect.size.y = 224
 	$ColorRect.position.y = 360
 	
@@ -91,3 +97,45 @@ func getGameScoreAndReset(gameOverScore):
 	
 	$ColorRect/scoreCalculationLabel.text = "Браво, сега можеш да купиш\n"
 	$ColorRect/scoreCalculationLabel.text += str((float(gameOverScore)/3692000)*100).pad_decimals(3) + "% од скопски стан"
+
+func _on_get_request_completed(_result, _response_code, _headers, body):
+	$ColorRect/leaderBoardScroll/leaderBoardLabel.text = ""
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	var brojac = 1
+	
+	if json != null:
+		lastPlaceScore = json[len(json)-1]["playerScore"]
+		
+		for row in json:
+			$ColorRect/leaderBoardScroll/leaderBoardLabel.text += str(brojac) + ". " + row["playerName"] + "    Score: " + str(row["playerScore"]) + "\n"
+			brojac += 1
+	
+
+func _on_post_request_completed(_result, _response_code, _headers, body):
+	pass
+
+
+func _on_refresh_button_pressed():
+	$ColorRect/RefreshButton.disabled = true
+	if !$HTTPRequest.request_completed.is_connected(refreshLeaderboard):
+		$HTTPRequest.request_completed.connect(refreshLeaderboard)
+	$HTTPRequest.request("https://nagrujofunkcijata.azurewebsites.net/api/GetRows")
+	await get_tree().create_timer(10).timeout 
+	$ColorRect/RefreshButton.disabled = false
+	
+func refreshLeaderboard(_result, _response_code, _headers, body):
+	$ColorRect/leaderBoardScroll/leaderBoardLabel.text = ""
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	var brojac = 1
+	
+	if !json == null:
+		lastPlaceScore = json[len(json)-1]["playerScore"]
+		
+		for row in json:
+			$ColorRect/leaderBoardScroll/leaderBoardLabel.text += str(brojac) + ". " + row["playerName"] + "    Score: " + str(row["playerScore"]) + "\n"
+			brojac += 1
+
+
+func _on_visibility_changed():
+	$ColorRect/EnterUsernameButton.disabled = false
+	$ColorRect/EnterScoreButton.disabled = false
